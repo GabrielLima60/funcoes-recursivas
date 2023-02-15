155672a6db4f0e8d0b4123da965e396198abfec8
Gabriel Lima <73233318+GabrielLima60@users.noreply.github.com>
2023-02-15 11:52:16 -0300

    Add files via upload

---
+++ funções recursivas/Atividade1.java
@@ -0,0 +1,13 @@
+public class Atividade1 {
+    
+    public static void recursaoCrescente(int numero) {
+        if(numero >= 0) {
+            recursaoCrescente(numero-1);
+            System.out.println(numero);
+        }
+    }
+    
+    public static void main(String args[]) {
+        recursaoCrescente(7);
+    }
+}
